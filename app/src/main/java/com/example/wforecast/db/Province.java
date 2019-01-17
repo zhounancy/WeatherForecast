83efb1557a220a4ebfeb81f398940740caaecd9e
Nance <tony@gmail.com>
2019-01-17 13:35:03 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/example/wforecast/db/Province.java
@@ -0,0 +1,33 @@
+package com.example.wforecast.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String ProvinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return ProvinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        ProvinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
